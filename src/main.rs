7a2de15ede6a6b6a7baab15e7c21155757b86847
Andrew Bell <andrew.bell012276@gmail.com>
2023-04-15 22:01:59 +0100

    use config file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+extern crate xdg;
 use std::error::Error;
+use std::fs;
 use std::future::pending;
 use std::sync::mpsc;
 use std::sync::Mutex;
 use std::thread;
 
 use clap::Parser;
+use serde::Deserialize;
 use zbus::ConnectionBuilder;
 
 use crate::frames::Frames;
@@ -13,7 +16,7 @@ mod frames;
 mod interface;
 mod worker;
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Deserialize)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Number of LEDs in the strip
@@ -27,14 +30,26 @@ struct Args {
     sleep_duration: u64,
 }
 
+fn get_args_from_config_file() -> Option<Args> {
+    let base_dir = xdg::BaseDirectories::new().ok()?;
+    let file = base_dir.find_config_file("config.toml")?;
+    let contents = fs::read_to_string(file).ok()?;
+    toml::to_string(&contents)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let Args {num_leds, clock_rate, sleep_duration} = Args::parse();
+    let args_from_config = get_args_from_config_file();
+    let args_from_cli = Args::parse();
     let (job_tx, job_rx) = mpsc::channel();
     let (interrupt_tx, interrupt_rx) = mpsc::channel();
     #[allow(unused_must_use)]
     thread::spawn(move || {
-        worker::update_leds(&mut Frames::new(num_leds, clock_rate, sleep_duration), job_rx, interrupt_rx);
+        worker::update_leds(
+            &mut Frames::new(num_leds, clock_rate, sleep_duration),
+            job_rx,
+            interrupt_rx,
+        );
     });
     let inst = interface::RustApa102 {
         frames: Frames::new(num_leds, clock_rate, sleep_duration),
