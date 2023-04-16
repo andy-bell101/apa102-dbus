8dca1651347836b61204c1f68aee823f7134b617
Andrew Bell <andrew.bell012276@gmail.com>
2023-04-16 12:05:04 +0100

    add more interface methods

--- src/interface.rs
+++ src/interface.rs
@@ -32,5 +32,30 @@ impl RustApa102 {
         Ok(())
     }
 
-    // TODO: flash, flash_hex, pulse, pulse_hex, rainbow, transition_hex
+    fn flash(&mut self, led: LEDState) -> Result<(), zbus::fdo::Error> {
+        self.transition(vec![led], false)
+    }
+
+    fn pulse(&mut self, led: LEDState) -> Result<(), zbus::fdo::Error> {
+        self.transition(vec![led, LEDState::new(0, 0, 0, 0, led.time)], true)
+    }
+
+    fn clear(&mut self) -> Result<(), zbus::fdo::Error> {
+        self.transition(vec![LEDState::new(0, 0, 0, 0, 1.0)], false)
+    }
+
+    fn rainbow(&mut self, time: f32, repeat: bool) -> Result<(), zbus::fdo::Error> {
+        let v = vec![
+            LEDState::new(255, 0xff, 0x00, 0x00, time), // red
+            LEDState::new(255, 0xff, 0xa5, 0x00, time), // orange
+            LEDState::new(255, 0xff, 0xff, 0x00, time), // yellow
+            LEDState::new(255, 0x00, 0x80, 0x00, time), // green
+            LEDState::new(255, 0x00, 0x00, 0xff, time), // blue
+            LEDState::new(255, 0x4b, 0x00, 0x82, time), // indigo
+            LEDState::new(255, 0xee, 0x82, 0xee, time), // violet
+        ];
+        self.transition(v, repeat)
+    }
+
+    // TODO: flash_hex, pulse_hex, rainbow, transition_hex
 }
